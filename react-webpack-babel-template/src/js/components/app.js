64f19c1634a313f21925e70dfa23faaba51dd457
techieVignesh <r.vignesh149@gmail.com>
2018-09-01 13:44:26 +0530

    react-webpack-babel-template

---
+++ react-webpack-babel-template/src/js/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component{
+	constructor(){
+		super();
+	}
+	render(){
+		return (<p>Hello React on local 2 </p>);
+	}
+}
+
+export default App;
